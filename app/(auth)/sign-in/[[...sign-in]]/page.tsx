a651371cd78ed7d83fcb06535d05f6048d4b9ef8
ugcinquaries-star <ugc.inquaries@gmail.com>
2026-06-06 08:45:50 +0200

    Add forceRedirectUrl prop to SignIn component

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function SignInPage() {
   return (
     <div style={{ minHeight: '100vh', background: '#06060b', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <SignIn />
+      <SignIn forceRedirectUrl="/dashboard" />
     </div>
   );
 }
